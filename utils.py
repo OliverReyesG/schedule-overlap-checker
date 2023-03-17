b6f625a0a374d75d78f5991f1a01f67ae6fc0fa1
oliverreyesg <ing.oliverreyesgutierrez@gmail.com>
2023-03-17 16:27:04 -0400

    Implement pending features

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 from abc import ABC
+from typing import List
+import re
+from models.employee import Employee
+from models.schedule import ScheduleRecord
 
 class FileParser(ABC):
     def __read_file(self, path):
@@ -13,10 +17,20 @@ class EmployeeScheduleParser(FileParser):
     def __init__(self):
         pass
 
-    # Defined for future implementation
-    def __read_file(self, path: str) -> str:
-        pass
+    
+    def __read_file(self, path: str) -> List[str]:
+        with open(path) as f:
+            return f.readlines()
 
-    # Defined for future implementation
-    def parse(self, path: str) -> dict:
-        pass
+
+    def parse(self, path: str) -> List[Employee]:
+        employees = []
+        file = self.__read_file(path)
+        name_pattern = r'([A-Z]+)='
+        schedule_record_pattern = r'(([A-Z]{2})([\d]{2}:[\d]{2})\s*-\s*([\d]{2}:[\d]{2}))'
+        for line in file:
+            employee_name = re.search(name_pattern, line).group(1)
+            raw_schedule_records = re.findall(schedule_record_pattern, line)
+            schedule = [ScheduleRecord(day=record[1], arrival_time=record[2], leave_time=record[3]) for record in raw_schedule_records]
+            employees.append(Employee(name=employee_name, schedule=schedule))
+        return employees
