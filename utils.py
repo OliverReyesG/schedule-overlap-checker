1964bb2880a63d3484f59576bacb4e767ec4ad70
oliverreyesg <ing.oliverreyesgutierrez@gmail.com>
2023-03-20 12:20:09 -0400

    Move overlap checking logic to reports module

--- utils.py
+++ utils.py
@@ -6,10 +6,10 @@ from models.schedule import ScheduleRecord
 
 
 class FileParser(ABC):
-    def __read_file(self, path):
+    def __read_file(self, path: str):
         pass
 
-    def parse(self, path):
+    def parse(self, path: str) -> List[Employee]:
         pass
 
 
@@ -30,17 +30,3 @@ class EmployeeScheduleParser(FileParser):
             schedule_records = {f"{day}": ScheduleRecord(arrival_time=arrival_time, leave_time=leave_time) for (match, day, arrival_time, leave_time) in raw_schedule_records}
             employees.append(Employee(name=employee_name, schedule_records=schedule_records))
         return employees
-
-
-def check_time_overlap(record1: ScheduleRecord, record2: ScheduleRecord):
-    if record1.arrival_time >= record2.arrival_time and record1.leave_time <= record2.leave_time:
-        return True
-    return False
-
-
-def count_schedule_overlap(schedule1: dict, schedule2: dict):
-    overlap_count = 0
-    for key in schedule1.keys():
-        if key in schedule2.keys() and check_time_overlap(schedule1[key], schedule2[key]):
-            overlap_count += 1
-    return overlap_count
